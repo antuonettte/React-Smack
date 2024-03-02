db7227e110a0423b561621c447a16f5b760783fe
Antonio Turner <tonyturner1399@gmail.com>
2024-03-02 01:08:37 -0500

    Update firebase.js
    removed api key

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import 'firebase/auth';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBGgykTxNqpU5lRpnBnOA5D2J3V2ojvc10",
+    apiKey: "",
     authDomain: "smack-f611d.firebaseapp.com",
     projectId: "smack-f611d",
     storageBucket: "smack-f611d.appspot.com",
-    messagingSenderId: "844668542733",
-    appId: "1:844668542733:web:b93f48dafc63e2ae1d1a44",
+    messagingSenderId: "",
+    appId: "",
     measurementId: "G-RK89MLHXP0"
     // apiKey: "AIzaSyBGgykTxNqpU5lRpnBnOA5D2J3V2ojvc10",
     // authDomain: "smack-f611d.firebaseapp.com",
